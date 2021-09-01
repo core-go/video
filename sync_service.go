8b8b2a1b3733d4590298f46f4032c4b183523730
vanhop993 <71499163+vanhop993@users.noreply.github.com>
2021-09-01 16:22:49 +0700

    video v1

---
+++ sync_service.go
@@ -0,0 +1,13 @@
+package video
+
+import (
+	"context"
+)
+
+type SyncService interface {
+	SyncChannel(ctx context.Context, channelId string) (int, error)
+	SyncChannels(ctx context.Context, channelIds []string) (int, error)
+	SyncPlaylist(ctx context.Context, playlistId string, level *int) (int, error)
+	SyncPlaylists(ctx context.Context, playlistIds []string,level int) (int,error)
+	GetSubscriptions(ctx context.Context, channelId string) ([]Channel, error)
+}
